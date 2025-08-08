0a1e4f297f2ffd43ad1729db3aeab06320becc8f
Lucas Vinicyus <sanches.anacleto@escola.pr.gov.br>
2025-08-08 12:39:05 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,15 +3,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyDB8b3Za9gkNY4hypl27RXjFmKN-B2Ry-U",
   authDomain: "game-a0337.firebaseapp.com",
   projectId: "game-a0337",
-  storageBucket: "game-a0337.firebasestorage.app",
+  storageBucket: "game-a0337.appspot.com", // corrigido
   messagingSenderId: "623161656290",
   appId: "1:623161656290:web:539fad00954b022c06efbb",
   measurementId: "G-G197MN430S"
 };
 
-// Inicializa o Firebase com SDK compatível (para uso com HTML/JS simples)
+// Inicializa o Firebase (modo compatível com v8)
 firebase.initializeApp(firebaseConfig);
 
-// Exporta os serviços de autenticação e banco de dados Firestore
+// Serviços
 const auth = firebase.auth();
 const db = firebase.firestore();
